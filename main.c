6d1d65bc7bce147ed68803c9ef62a9574d6aa1ea
Suman Sahoo <ssamsomu@gmail.com>
2022-07-27 18:21:01 +0530

    what to do

---
+++ main.c
@@ -0,0 +1,29 @@
+
+void start();
+
+int main(){
+
+    start();
+}
+
+void start(){
+
+    //OPTIONS
+
+    // 1 show existing contacts- 
+    //     :sort by name 
+    //     :sort by number
+    
+    
+    // 2 add a new contact
+    // 2 delete a contact
+    // 3 edit a contact
+    // {these will make changes to the LL or array}
+
+    // 4 search 
+    //      :should be done by both name and number
+
+
+
+}
+
