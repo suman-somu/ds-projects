8b006ba42558eae9e2f325ce9d4884ebe5993e6a
Baibhav Kumar <thebaibhavkumar122003@gmail.com>
2022-07-30 14:01:40 +0530

    Added Title_Screen function.

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
 
-void start();
+
+void gotoxy(int x, int y);
+
+void Title_Screen();
 
 int main(){
-
-    start();
+    Title_Screen();
 }
 
-void start(){
-
-    //OPTIONS
-
-    // 1 show existing contacts- 
-    //     :sort by name 
-    //     :sort by number
-    
-    
-    // 2 add a new contact
-    // 2 delete a contact
-    // 3 edit a contact
-    // {these will make changes to the LL or array}
-
-    // 4 search 
-    //      :should be done by both name and number
-
-
+void Title_Screen(){
+    /*
+    This function presents the first screen before the audience. It mentions the project name and developers name.
+    */
+    system("cls");
+    gotoxy(76, 16);
+    printf("Project Phone Book");
+    gotoxy(76, 20);
+    printf("Developed by:");
+    gotoxy(76, 23);
+    printf("Baibhav Kumar, ID: B421016.");
+    gotoxy(76, 25);
+    printf("Hemant Sah, ID: B421025");
+    gotoxy(76, 27);
+    printf("Suman Sahoo, ID: B421056");
 
 }
 
+void gotoxy(int x, int y)
+{
+    /*
+    The gotoxy() function places the cursor at the desired location on the screen.
+    SetConsoleCursorPosition is present in windows.h header file.
+    */
+    COORD c;
+    c.X = x;
+    c.Y = y;
+ 
+    SetConsoleCursorPosition(
+        GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
